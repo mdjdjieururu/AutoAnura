d6fb6d3438a6c278923ed1e4ec3b8df359849b09
mallumoviesearch <103921432+mallumoviesearch@users.noreply.github.com>
2022-12-13 20:56:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '16138426'))
 API_HASH = environ.get('API_HASH', '2f6d827314b217206123c543a42bb577')
-BOT_TOKEN = environ.get('BOT_TOKEN', '2121450006:AAHIjYUNxw4SGyuFBbLKW3rn4QWThFu3uZI')
+BOT_TOKEN = environ.get('BOT_TOKEN', '2121450006:2121450006:AAGGODl5f-wGSrYdX8nr507w0gGOFj6pLlY')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
